55b626d9fb5322f090a5af2d6bd27b993189b4d2
Kingsley Solomon <kinxly@gmail.com>
2024-02-14 16:40:45 +0100

    fix: added local font to app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,48 +1,44 @@
 /** @type {import("tailwindcss").Config} */
 export default {
-	content:[
-		"./index.html",
-		"./src/**/*.{js,ts,jsx,tsx}",
-	],
-	theme:{
-		//color settings
-		colors:{
-			red:{
-				100:"",
-				200:"",
-				300:"",
-				400:"",
-				500:"#FE0000",
-			},
-			white:{
-				100:"",
-				200:"",
-				300:"",
-				400:"",
-				500:"#FFFFFF",
-			},
-			black:{
-				100:"#26282E",
-				200:"",
-				300:"",
-				400:"",
-				500:"#000000",
-			},
-		},
-		//  font settings
-		// fontFamily:{
-		// 	"bricolage":[ "Bricolage Grotesque", "sans-serif" ],
-		// 	"barlow-semi-condensed":[ "Barlow Semi Condensed", "sans-serif" ],
-		// },
-		//breakpoints
-		screenSize:{
-			sm:"480px",
-			md:"768px",
-			lg:"976px",
-			xl:"1440px",
-		}
-		,
-		extend:{},
-	},
-	plugins:[],
-}
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        //color settings
+        colors: {
+            red: {
+                100: "",
+                200: "",
+                300: "",
+                400: "",
+                500: "#FE0000",
+            },
+            white: {
+                100: "",
+                200: "",
+                300: "",
+                400: "",
+                500: "#FFFFFF",
+            },
+            black: {
+                100: "#26282E",
+                200: "",
+                300: "",
+                400: "",
+                500: "#000000",
+            },
+        },
+        //  font settings
+        // fontFamily:{
+        // 	"bricolage":[ "Bricolage Grotesque", "sans-serif" ],
+        // 	"barlow-semi-condensed":[ "Barlow Semi Condensed", "sans-serif" ],
+        // },
+        //breakpoints
+        screenSize: {
+            sm: "480px",
+            md: "768px",
+            lg: "976px",
+            xl: "1440px",
+        },
+        extend: {},
+    },
+    plugins: [],
+};
