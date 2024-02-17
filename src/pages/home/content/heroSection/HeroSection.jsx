75ccbf4f9b0e179abe54700c3e742f02a84610f7
Daniel <danielkore1234@gmail.com>
2024-02-17 19:28:07 +0100

    update

--- src/pages/home/content/heroSection/HeroSection.jsx
+++ src/pages/home/content/heroSection/HeroSection.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import ContactBtn from  '../../../../components/ContactBtn/ContactBtn';
 import './HeroSection.scss'
 
 const HeroSection = () => {
   return (
     <div>
        <div className='responsive w-11/12 '>
-       <div className='hero-content flex justify-start items-center'>
-           <div className=''>
-           <h1 className='large-text text-white-100'><span className='text-red-400 large-text'>Illuminate</span> Your Home With Suraab Electrical Services</h1>
+       <div className='absolute top-2 flex justify-center lg:justify-start pt-8 '>
+           <div className='w-full lg:w-1/2  py-10'>
+           <h1 className='large-text text-white-100 text-center lg:text-start'><span className='text-red-400 large-text text-center lg:text-start'>Illuminate</span> Your Home With Suraab Electrical Services</h1>
+           <p className='py-5 medium-text text-white-100 text-center lg:text-start'>At Suraab, we go beyond the wires and circuits – we empower homes with solutions that prioritize efficiency, reliability, and your peace of mind.</p>
+           <div className='flex justify-center lg:justify-start items-center lg:items-start'>
+           <ContactBtn/>
+           </div>
            </div>
            <div>
-
            </div>
         </div>
        </div>
