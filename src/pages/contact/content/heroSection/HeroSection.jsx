f800bfdd6b1372318295c7fbddd29c27df8dc28f
AduragbemiShobowale <shobowaleaduragbemi24@gmail.com>
2024-02-17 17:17:42 +0100

    complete contact form

--- src/pages/contact/content/heroSection/HeroSection.jsx
+++ src/pages/contact/content/heroSection/HeroSection.jsx
@@ -3,7 +3,7 @@ import arrow from "../../../../assets/arrow.png";
 
 const HeroSection = () => {
   return (
-    <div className="contact-bg">
+    <div className="contact-bg flex items-center justify-center">
       <div className="py-10 text-center">
         <h1 className="text-white-100 large-text">Contact</h1>
         <span className="flex justify-center items-center gap-2 py-5">
