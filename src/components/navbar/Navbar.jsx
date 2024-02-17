596a071b70a7f764f559faf314c95af94778e3df
Daniel <danielkore1234@gmail.com>
2024-02-17 09:31:52 +0100

    commiting changes before merging

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import './navbar.css'
+import './Navbar.scss'
 import ContactBtn from "../ContactBtn/ContactBtn";
 import { Sling as Hamburger } from 'hamburger-react'
 
