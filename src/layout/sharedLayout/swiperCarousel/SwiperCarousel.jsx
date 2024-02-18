bc0728e39103007957b429cc742dbea0e1af31c0
Daniel <danielkore1234@gmail.com>
2024-02-18 18:53:35 +0100

    configured vercel to avaoid 404 page

--- src/layout/sharedLayout/swiperCarousel/SwiperCarousel.jsx
+++ src/layout/sharedLayout/swiperCarousel/SwiperCarousel.jsx
@@ -1,32 +1,41 @@
-import './SwiperCarousel.scss'
-import 'swiper/css';
-import 'swiper/css/pagination';
-import { useRef, useState } from 'react';
+import "./SwiperCarousel.scss";
+import "swiper/css";
+import "swiper/css/pagination";
+import { useRef, useState } from "react";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
-import { Pagination, Autoplay } from 'swiper/modules';
-import slide1 from '../../../assets/slide1.png'
-import slide2 from '../../../assets/slide2.png'
-import slidethree from '../../../assets/slide3.png'
-import panorama from '../../../assets/paranoma.png'
-
+import { Pagination, Autoplay } from "swiper/modules";
+import slide1 from "../../../assets/slide1.png";
+import slide2 from "../../../assets/slide2.png";
+import slidethree from "../../../assets/slide3.png";
+import panorama from "../../../assets/paranoma.png";
 
 const SwiperCarousel = () => {
   return (
-    <div className='py-10 relative flex justify-center items-center'>
-        <img src={panorama} alt="" className='absolute top-1 z-40 w-full' />
-        <img src={panorama} alt="" className='absolute bottom-1 z-40 w-full' />
+    <div className="py-10 relative flex justify-center items-center">
+      <img src={panorama} alt="" className="absolute top-1 z-40 w-full" />
+      <img src={panorama} alt="" className="absolute bottom-1 z-40 w-full" />
 
-        <Swiper 
-        // slidesPerView={3}
-        // spaceBetween={30}
-        // pagination={{
-        //   clickable: true,
-        // }}
-        // modules={[Pagination]}
-        // className="mySwiper"
-        slidesPerView={3}
+      <Swiper
+
+breakpoints={{
+    320: {
+      slidesPerView: 1,
+    },
+    375: {
+      slidesPerView: 1,
+    },
+    480: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: { slidesPerView: 3 },
+    1440: { slidesPerView: 3 },
+  }}
+
         spaceBetween={40}
         centeredSlides={true}
         autoplay={{
@@ -37,33 +46,31 @@ const SwiperCarousel = () => {
           clickable: true,
         }}
         navigation={true}
-        modules={[Autoplay, ]}
+        modules={[Autoplay]}
         className="mySwiper"
+      
       >
         <SwiperSlide>
-        <img src={slide1} alt="" />
+          <img src={slide1} alt="" />
         </SwiperSlide>
         <SwiperSlide>
-        <img src={slide2} alt="" />
+          <img src={slide2} alt="" />
         </SwiperSlide>
         <SwiperSlide>
-        <img src={slidethree} alt="" />
+          <img src={slidethree} alt="" />
         </SwiperSlide>
         <SwiperSlide>
-        <img src={slide1} alt="" />
+          <img src={slide1} alt="" />
         </SwiperSlide>
         <SwiperSlide>
-        <img src={slide2} alt="" />
+          <img src={slide2} alt="" />
         </SwiperSlide>
         <SwiperSlide>
-        <img src={slidethree} alt="" />
+          <img src={slidethree} alt="" />
         </SwiperSlide>
-       
-     
-
       </Swiper>
     </div>
-  )
-}
+  );
+};
 
-export default SwiperCarousel
+export default SwiperCarousel;
