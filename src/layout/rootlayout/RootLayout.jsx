534e4219be561e54547b90f86fb278d78e03543d
Daniel <danielkore1234@gmail.com>
2024-02-19 13:19:10 +0100

    refactor: fixed navbar

--- src/layout/rootlayout/RootLayout.jsx
+++ src/layout/rootlayout/RootLayout.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom"
 import Navbar from '../../components/navbarComponent/Navbar'
 import Footer from '../../components/footerComponent/Footer'
+import './RootLayout.scss'
 
 const RootLayout = () => {
   return (
     <div>
       <Navbar/>
-       <div className="">
+       <div className="layout">
        <Outlet/>
        </div>
         <Footer/>
