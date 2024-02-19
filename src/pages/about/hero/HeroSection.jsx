c38826a23a5c5abfba11f7357f9c37e1b065205e
Daniel <danielkore1234@gmail.com>
2024-02-19 21:06:42 +0100

    update: added breadcrum functionality

--- src/pages/about/hero/HeroSection.jsx
+++ src/pages/about/hero/HeroSection.jsx
@@ -1,5 +1,6 @@
 import "./HerosSection.scss";
 import img from "../../../assets/arrow.png";
+import { Link } from "react-router-dom";
 
 const HeroSection = () => {
   return (
@@ -7,7 +8,9 @@ const HeroSection = () => {
       <div className="py-10 text-center">
         <h1 className="text-white-100 large-text pt-24">About</h1>
         <span className="flex justify-center items-center gap-2 py-5 ">
+          <Link to="/">
           <p className="text-black-100">Home</p>
+          </Link>
           <span>
             <img src={img} alt="" />
           </span>
