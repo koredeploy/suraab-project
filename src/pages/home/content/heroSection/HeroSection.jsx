6c7902fbc75cd7912c9133294e3c1f7b619da0b6
Daniel <danielkore1234@gmail.com>
2024-02-20 01:40:25 +0100

    update: added backdrop images

--- src/pages/home/content/heroSection/HeroSection.jsx
+++ src/pages/home/content/heroSection/HeroSection.jsx
@@ -5,11 +5,11 @@ const HeroSection = () => {
   return (
     <div>
        <div className=' flex justify-center items-center'>
-       <div className=' absolute  left-0 right-0 flex top-24 lg:top-28 z-40 justify-center items-center lg:justify-start'>
+       <div className=' absolute  left-0 right-0 flex top-24 lg:top-24 z-40 justify-center items-center lg:justify-start'>
           <div className='w-11/12 mx-auto responsive '>
-          <div className= 'w-full pt-5 '>
-           <h1 className='lg:w-1/2 large-text text-white-100 text-center lg:text-start  pt-3  md:pt-10'><span className='large-text text-red-400 text-center lg:text-start 2xl:text-4xl'>Illuminate</span> Your Home  With Suraab Electrical Services</h1>
-           <p className='lg:w-1/2 py-5 text-white-100 text-center lg:text-start '>At Suraab, we go beyond the wires and circuits – we empower homes with solutions that prioritize efficiency, reliability, and your peace of mind.</p>
+          <div className= 'w-full pt-5 lg:w-1/2 '>
+           <h1 className='w-full hero-text text-white-100 text-center lg:text-start pt-3 md:pt-10'><span className='hero-text text-red-400 text-center lg:text-start'>Illuminate</span> Your Home  With Suraab Electrical Services</h1>
+           <p className='py-5 text-white-100 text-center lg:text-start  '>At Suraab, we go beyond the wires and circuits – we empower homes with solutions that prioritize efficiency, reliability, and your peace of mind.</p>
            <div className='flex justify-center lg:justify-start items-center lg:items-start '>
            <ContactBtn/>
            </div>
