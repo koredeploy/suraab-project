9f585879bbde09b2b96768443bfb4a3926ea8f08
AduragbemiShobowale <shobowaleaduragbemi24@gmail.com>
2024-02-20 10:47:30 +0100

    nav changed

--- src/pages/service/content/hero/HeroSection.jsx
+++ src/pages/service/content/hero/HeroSection.jsx
@@ -5,17 +5,18 @@ import './HeroSection.scss'
 const HeroSection = ({img}) => {
   return (
     <div className='service-bg flex items-center justify-center '>
-    <div className='py-10 text-center'>
-        <h1 className=' large-text text-red-400 large-text pt-24'>Our Service</h1>
-        <div className='flex justify-center items-center gap-2 py-5 '>
+<div className="py-10 text-center">
+        <h1 className="text-white-100 large-text pt-24 ">Service</h1>
+        <span className="flex justify-center items-center gap-2 py-5 ">
           <Link to="/">
-          <p className='text-3xl text-black-100'>Home</p>
+          <p className="text-black-100 medium-text">Home</p>
           </Link>
-       
-        <span><img src={img} className='' alt="" /></span>
-        <h5 className='text-white-100 text-3xl'>Service</h5>
-        </div>
-    </div>
+          <span>
+            <img src={img} alt="" />
+          </span>
+          <h5 className="text-white-100 ">Service</h5>
+        </span>
+      </div>
 </div>
   )
 }
