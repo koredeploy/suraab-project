95af8b545ad385eda47293969cf6b3d196e42215
Daniel <danielkore1234@gmail.com>
2024-02-20 11:45:03 +0100

    increased background min-heiht for about page background image

--- src/pages/about/hero/HeroSection.jsx
+++ src/pages/about/hero/HeroSection.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const HeroSection = () => {
   return (
     <div className="about-bg flex items-center justify-center ">
-      <div className="py-10 text-center">
+      <div className="py-10 lg:py-16 xl:py-20 text-center">
         <h1 className="text-white-100 large-text pt-24 ">About</h1>
         <span className="flex justify-center items-center gap-2 py-5 ">
           <Link to="/">
