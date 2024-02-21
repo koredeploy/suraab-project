f970b77fc1c294f36afe5203c8d936bd72700b93
AduragbemiShobowale <shobowaleaduragbemi24@gmail.com>
2024-02-21 11:52:03 +0100

    fixed error page

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,34 @@
-import {
-  Route,
-  RouterProvider,
+import { 
+  Route, 
+  RouterProvider, 
   createBrowserRouter,
-  createRoutesFromElements,
+  createRoutesFromElements  
 } from "react-router-dom";
+
 import RootLayout from "./layout/rootlayout/RootLayout";
 import HomePage from "./pages/home/HomePage";
-import ServicePage from "./pages/service/ServicePage";
+import ServicePage from "./pages/service/ServicePage"; 
 import ContactUsPage from "./pages/contact/ContactUsPage";
 import AboutPage from "./pages/about/AboutPage";
 import ErrorPage from "./pages/404/ErrorPage";
-// import FadedCarousel from "./layout/sharedLayout/fadedCarousel/FadedCarousel";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route element={<RootLayout />}>
-      <Route index path="/" element={<HomePage />} />
-      <Route path="/aboutus" element={<AboutPage />} />
-      <Route path="/service" element={<ServicePage />} />
-      {/* <Route path="/carousel" element={<FadedCarousel  />} /> */}
-      <Route path="/contactus" element={<ContactUsPage />} />
-    </Route>
-
-
+    <>
+      <Route element={<RootLayout />}>
+        <Route index path="/" element={<HomePage />} />
+        <Route path="/aboutus" element={<AboutPage />} />  
+        <Route path="/service" element={<ServicePage />} />
+        <Route path="/contactus" element={<ContactUsPage />} />
+      </Route>
+      
+      <Route path="*" element={<ErrorPage />} /> 
+    </>
   )
 );
+
 function App() {
-  return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />; 
 }
 
 export default App;
