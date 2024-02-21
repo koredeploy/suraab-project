af63f85d2c0df4c28d7fcb4e37fbb1336f539fc7
Daniel <danielkore1234@gmail.com>
2024-02-21 17:57:33 +0100

    update-changes

--- src/pages/service/content/hero/HeroSection.jsx
+++ src/pages/service/content/hero/HeroSection.jsx
@@ -6,7 +6,7 @@ const HeroSection = ({img}) => {
   return (
     <div className='service-bg flex items-center justify-center '>
 <div className="py-10 text-center">
-        <h1 className="text-white-100 large-text pt-24 ">Service</h1>
+        <h1 className="text-white-100 large-text pt-24 ">Services</h1>
         <div className="flex justify-center items-center gap-2 py-5 ">
           <Link to="/">
             <p className="text-black-100 text-3xl font-medium pb-2">Home</p>
@@ -14,7 +14,7 @@ const HeroSection = ({img}) => {
           <span>
             <img src={img} alt="" />
           </span>
-          <h5 className="text-white-100 text-3xl font-semibold">Service</h5>
+          <h5 className="text-white-100 text-3xl font-semibold">Services</h5>
         </div>
       </div>
 </div>
